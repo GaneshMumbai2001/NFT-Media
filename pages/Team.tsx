1f9b7717ef019401f6a0b090c0e7bce419bbc6cf
GaneshMumbai2001 <103201642+GaneshMumbai2001@users.noreply.github.com>
2024-05-03 15:44:27 +0530

    updated the team profile

--- pages/Team.tsx
+++ pages/Team.tsx
@@ -5,7 +5,7 @@ const people = [
       name: 'Leslie Alexander',
       role: 'Co-Founder / CEO',
       imageUrl:
-        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+        'https://blogger.googleusercontent.com/img/a/AVvXsEjQYAgokk-1eGmchKwP4qXkUn2HUNKZwFqBbPUVmoSY_DnzDDrl4raZssewY1cFfDx9nIYznuImcnyhocjWgd2TuVdTtT61-N-RUNwlcWrbg0FQLcBzew-jhdv015JGzXmuJCgvwP7oFtPEKA2A8WuEXhq5_v2L_SpeY8Z4XsOoKKSzv5Pnce0eaYlRyG4',
       bio: 'Ultricies massa malesuada viverra cras lobortis. Tempor orci hac ligula dapibus mauris sit ut eu. Eget turpis urna maecenas cras. Nisl dictum.',
       twitterUrl: '#',
       linkedinUrl: '#',
