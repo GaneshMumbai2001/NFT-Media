caea24356936d28282827c4512378cc7149454ed
GaneshMumbai2001 <103201642+GaneshMumbai2001@users.noreply.github.com>
2024-05-04 10:57:17 +0530

    updated the team profile

--- pages/Team.tsx
+++ pages/Team.tsx
@@ -2,22 +2,22 @@ import Navbar from "./Components/Navbar"
 
 const people = [
     {
-      name: 'Leslie Alexander',
-      role: 'Co-Founder / CEO',
+      name: 'Ganesh',
+      role: 'Full Stack & Blockchain Developer',
       imageUrl:
         'https://blogger.googleusercontent.com/img/a/AVvXsEjQYAgokk-1eGmchKwP4qXkUn2HUNKZwFqBbPUVmoSY_DnzDDrl4raZssewY1cFfDx9nIYznuImcnyhocjWgd2TuVdTtT61-N-RUNwlcWrbg0FQLcBzew-jhdv015JGzXmuJCgvwP7oFtPEKA2A8WuEXhq5_v2L_SpeY8Z4XsOoKKSzv5Pnce0eaYlRyG4',
-      bio: 'Ultricies massa malesuada viverra cras lobortis. Tempor orci hac ligula dapibus mauris sit ut eu. Eget turpis urna maecenas cras. Nisl dictum.',
-      twitterUrl: '#',
-      linkedinUrl: '#',
+      bio: 'I am a full-stack and blockchain developer with a year of experience in both development and technical writing. My expertise includes building smart contracts and crafting clear, impactful technical content.',
+      twitterUrl: 'https://x.com/ganesh__mumbai?s=09',
+      linkedinUrl: 'https://www.linkedin.com/in/ganesh-mumbai-b863a3203?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
     },
     {
-        name: 'Leslie Alexander',
-        role: 'Co-Founder / CEO',
+        name: 'Kalpana',
+        role: 'UI & UX Designer',
         imageUrl:
           'https://blogger.googleusercontent.com/img/a/AVvXsEgmveWZPvUi2ixkdULa6_0hMAK0iDQdg-tX2x0FxgeRPmEKwHU_KxVUBeUCiQxclyC2sW6YVPlYeHNMVO5O1vZPxsbT649FnUgOHL2EKFvniEzIzMtkFsRKbaqZm9m2vgETdVm5N8JrKHXdX8-TVYn--RklVJkQGbGycVBUR1UYV5GH0qtpVZpcNoew7LM',
-        bio: 'Ultricies massa malesuada viverra cras lobortis. Tempor orci hac ligula dapibus mauris sit ut eu. Eget turpis urna maecenas cras. Nisl dictum.',
-        twitterUrl: '#',
-        linkedinUrl: '#',
+        bio: 'I am a UI/UX designer and front-end developer passionate about crafting engaging and visually appealing digital experiences. I specialize in creating intuitive interfaces and translating them into functional applications, focusing on optimizing user satisfaction and interaction.',
+        twitterUrl: 'https://x.com/Kalpana2622?t=zT7XqODexcGLIMrMRn8PtQ&s=08',
+        linkedinUrl: 'https://www.linkedin.com/in/kalpana-shanmugam-5b434524a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
       },
   ]
   
@@ -29,9 +29,8 @@ const people = [
           <div className="space-y-12 lg:grid lg:grid-cols-3 lg:gap-8 lg:space-y-0">
             <div className="space-y-5 sm:space-y-4">
               <h2 className="text-3xl font-extrabold tracking-tight sm:text-4xl">About Us</h2>
-              <p className="text-xl text-gray-500">
-                Nulla quam felis, enim faucibus proin velit, ornare id pretium. Augue ultrices sed arcu condimentum
-                vestibulum suspendisse. Volutpat eu faucibus vivamus eget bibendum cras.
+              <p className="text-xl text-gray-500" style={{ textAlign : "justify",width:"99%",marginLeft:"-41px" }}>
+              Welcome to NFT Media, where we revolutionize social media through Web3 technology.Our platform allows users to fully control their digital identity and content with unparalleled security and transparency. Here, you can mint NFTs, manage digital assets, and engage in a community-driven space with smart contracts ensuring every transaction is secure. Our innovative features make social media safer, enjoyable, and rewarding. Join us to experience a new era of social media where privacy, creativity, and user empowerment are prioritized.
               </p>
             </div>
             <div className="lg:col-span-2">
@@ -56,7 +55,7 @@ const people = [
                           </div>
                           <ul role="list" className="flex space-x-5">
                             <li>
-                              <a href={person.twitterUrl} className="text-gray-400 hover:text-gray-500">
+                              <a href={person.twitterUrl} className="text-blue-400 hover:text-blue-500">
                                 <span className="sr-only">Twitter</span>
                                 <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
                                   <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
@@ -64,7 +63,7 @@ const people = [
                               </a>
                             </li>
                             <li>
-                              <a href={person.linkedinUrl} className="text-gray-400 hover:text-gray-500">
+                              <a href={person.linkedinUrl} className="text-blue-400 hover:text-blue-500">
                                 <span className="sr-only">LinkedIn</span>
                                 <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
                                   <path
